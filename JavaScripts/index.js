e71b4e265795512989e0839f5a903a8f1f486e52
yasirupanduka99 <it20162382@my.sliit.lk>
2021-12-18 16:50:15 +0530

    Adding Background Music to the Home Page

---
+++ JavaScripts/index.js
@@ -0,0 +1,22 @@
+
+
+//Script For Audio Default Volume-->
+var audio = document.getElementById('backAudio');
+audio.volume = 0.19;
+
+//Script For Audio Volume Button-->
+var volBtn = document.getElementById('volumeBtn');
+var count = 0;
+
+function playPause(){
+    if(count == 0){
+        count = 1;
+        audio.play();
+        volBtn.innerHTML = '<i class="fas fa-volume-down"></i>';
+    }
+    else{
+        count = 0;
+        audio.pause();
+        volBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+    }
+}
