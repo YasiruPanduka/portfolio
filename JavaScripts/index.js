32e77bdb741c71157db6e61d6af570a1dd98ba9e
yasirupanduka99 <it20162382@my.sliit.lk>
2021-12-18 17:56:29 +0530

    adding css fOR bACKGROUND mUSIC pAUSE bUTTON

--- JavaScripts/index.js
+++ JavaScripts/index.js
@@ -2,21 +2,24 @@
 
 //Script For Audio Default Volume-->
 var audio = document.getElementById('backAudio');
-audio.volume = 0.19;
+audio.volume = 0.2;
 
 //Script For Audio Volume Button-->
 var volBtn = document.getElementById('volumeBtn');
+var disk = document.getElementById('disk');
 var count = 1;
 
 function playPause(){
     if(count == 0){
         count = 1;
         audio.play();
-        volBtn.innerHTML = '<i class="fas fa-volume-down"></i>';
+        volBtn.innerHTML = '<i class="fas fa-volume-down fa-2x"></i>';
+        disk.innerHTML = '<i class="fas fa-compact-disc fa-3x fa-spin" style="--fa-animation-duration: 15s;"></i>'
     }
     else{
         count = 0;
         audio.pause();
-        volBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+        volBtn.innerHTML = '<i class="fas fa-volume-mute fa-2x"></i>';
+        disk.innerHTML = '<i class="fas fa-compact-disc fa-3x"></i>'
     }
 }
